d059a154a2c50370ae7036fba1e4cd821d9c2eb0
Mahdi <mahdimey17@gmail.com>
2024-03-21 13:20:04 +0330

    created MenuItem after the data is loaded

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,15 +1,24 @@
-import { useLoaderData } from "react-router-dom"
-import { getMenu } from "../../services/apiRestaurant"
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
 function Menu() {
-  const menu =  useLoaderData()
+  const menu = useLoaderData();
   console.log(menu)
-  return <h1>Menu</h1>
+
+  return (
+    // <h1>MENU COMPONENT</h1>
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function Loader() {
-  const menu = await getMenu()
-  return menu
+  const menu = await getMenu();
+  return menu;
 }
 
-export default Menu
+export default Menu;
