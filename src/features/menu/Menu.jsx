8b5f3403b0b2319b35ceea8483a7d45474c669af
Mahdi <mahdimey17@gmail.com>
2024-03-21 15:41:34 +0330

    displayed loading indicator when app is loading

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -4,10 +4,8 @@ import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu)
 
   return (
-    // <h1>MENU COMPONENT</h1>
     <ul>
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import {formatCurrency} from '../../utils/helpers'
 
 function MenuItem({ pizza }) {
