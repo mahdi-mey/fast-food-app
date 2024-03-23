95242d9dd3734299b08c92d8d262f3f68080c45e
Mahdi <mahdimey17@gmail.com>
2024-03-23 16:15:31 +0330

    handled dummy phone number error in Form using useActionData hook

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect, useNavigation } from "react-router-dom"
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom"
 import { createOrder } from "../../services/apiRestaurant"
 createOrder
 
@@ -35,7 +35,9 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation()
   const isSubmitting = navigation.state === "submitting"
-  // const [withPriority, setWithPriority] = useState(false);
+
+  const formErrors = useActionData()
+
   const cart = fakeCart
 
   return (
@@ -52,6 +54,7 @@ function CreateOrder() {
           <label>Phone number</label>
           <div>
             <input type="tel" name="phone" required />
+            {formErrors?.phone && <p>{formErrors.phone}</p>}
           </div>
         </div>
 
@@ -94,6 +97,14 @@ export async function action({ request }) {
     priority: data.priority === "on",
   }
 
+  const errors = {}
+  if (!isValidPhone(order.phone)) {
+    errors.phone = "Please enter a valid phone number"
+  }
+
+  if (Object.keys(errors).length > 0) return errors
+
+  // if everything is ok then creates order and redericts
   const newOrder = await createOrder(order)
   return redirect(`/order/${newOrder.id}`)
 }
