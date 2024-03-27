f03af42d7814fb9e0352fb2bd31b51045f407c84
Mahdi <mahdimey17@gmail.com>
2024-03-27 11:18:50 +0330

    finished styling Orer and OrderItem

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom"
 import { getOrder } from "../../services/apiRestaurant"
+import OrderItem from "./OrderItem"
 
 // Test ID: IIDSAT
 
@@ -59,29 +60,47 @@ function Order() {
   const deliveryIn = calcMinutesLeft(estimatedDelivery)
 
   return (
-    <div>
-      <div>
-        <h2>Status</h2>
+    <div className="px-4 py-6 space-y-8">
+      <div className="flex items-center justify-between flex-wrap gap-3">
+        <h2 className="text-xl font-semibold">Order #{id} status</h2>
 
-        <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+        <div className="space-x-2">
+          {priority && (
+            <span className="bg-red-500 text-sm uppercase text-red-50 tracking-wide rounded-full py-2 px-2">
+              Priority
+            </span>
+          )}
+          <span className="bg-green-500 text-sm uppercase text-red-50 tracking-wide rounded-full py-2 px-2">
+            {status} order
+          </span>
         </div>
       </div>
 
-      <div>
-        <p>
+      <div className="flex items-center justify-between flex-wrap gap-3 bg-stone-200 px-6 py-5">
+        <p className="font-medium">
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
             : "Order should have arrived"}
         </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+        <p className="text-sm text-stone-500">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
       </div>
 
-      <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
+      <ul className="divide-stone-200 divide-y border-b border-t">
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.id} />
+        ))}
+      </ul>
+
+      <div className="space-y-2 bg-stone-200 px-6 py-5">
+        <p className="text-sm font-medium text-stone-600">
+          Price pizza: {formatCurrency(orderPrice)}
+        </p>
         {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+        <p className="font-bold">
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   )
--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable react/prop-types */
+import { formatCurrency } from "../../utils/helpers";
+
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
-      <div>
+    <li className="py-3">
+      <div className="flex items-center justify-between">
         <p>
-          <span>{quantity}&times;</span> {name}
+          <span className="font-bold">{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
