c81e0075a4dd6b80ac9118c553f2239e548aa457
Mahdi <mahdimey17@gmail.com>
2024-03-30 18:52:33 +0330

    built CartOverview by using redux in it

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,18 @@
+import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice"
 
 function CartOverview() {
+  const totalPrice = useSelector(getTotalCartPrice)
+  const totalQuantity = useSelector(getTotalCartQuantity)
+
+  if (!totalQuantity) return null
+
   return (
-    <div className="bg-stone-800 text-stone-200 text-sm uppercase px-6 py-4 sm:px6 md:text-base flex justify-between">
-      <p className="text-stone-300 font-semibold space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+    <div className="sm:px6 flex justify-between bg-stone-800 px-6 py-4 text-sm uppercase text-stone-200 md:text-base">
+      <p className="space-x-6 font-semibold text-stone-300">
+        <span>{totalQuantity} pizzas</span>
+        <span>${totalPrice}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,7 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    cart: []
+    cart: [],
+
+    // example structure
+    // {
+    //     pizzaId: 12,
+    //     name: 'Pepperoni',
+    //     quantity: 2,
+    //     unitPrice: 16,
+    //     totalPrice: 32
+    // }
 }
 
 const cartSlice = createSlice({
@@ -46,3 +55,14 @@ export const {
 } = cartSlice.actions
 
 export default cartSlice.reducer
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+    state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+    state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+    state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
