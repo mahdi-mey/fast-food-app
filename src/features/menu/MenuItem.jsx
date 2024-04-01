45c0130a2536bed75544efe69833cc655d10cfcb
Mahdi <mahdimey17@gmail.com>
2024-04-01 13:06:27 +0330

    added Delete button instead of Add To Cart after user added to cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
-import {useDispatch} from 'react-redux'
+import { useDispatch, useSelector } from "react-redux"
 import { formatCurrency } from "../../utils/helpers"
 import Button from "../../ui/Button"
-import { addItem } from '../cart/cartSlice'
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice"
+import DeleteItem from "../cart/DeleteItem"
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza
+  const currentQuantity = useSelector(getCurrentQuantityById(id))
+  const isInCart = currentQuantity > 0
   const dispatch = useDispatch()
 
   function addToCart() {
@@ -37,7 +40,10 @@ function MenuItem({ pizza }) {
           ) : (
             <p className="uppercase">Sold out</p>
           )}
-          {!soldOut && (
+
+          {isInCart && <DeleteItem pizzaId={id} />}
+
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={addToCart}>
               Add to cart
             </Button>
