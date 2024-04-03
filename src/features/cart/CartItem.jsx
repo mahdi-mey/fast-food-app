59ed10f0333cc775f797ab01fb3b06abf49b478a
Mahdi <mahdimey17@gmail.com>
2024-04-03 21:16:31 +0330

    implemented updating cart quantity function

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable react/prop-types */
+import { useSelector } from "react-redux"
 import { formatCurrency } from "../../utils/helpers"
 import DeleteItem from "./DeleteItem"
+import UpdateItemsQuantity from "./UpdateItemsQuantity"
+import { getCurrentQuantityById } from "./cartSlice"
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item
+
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId))
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -12,6 +17,10 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <UpdateItemsQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
+        />
         <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -4,6 +4,7 @@ import { formatCurrency } from "../../utils/helpers"
 import Button from "../../ui/Button"
 import { addItem, getCurrentQuantityById } from "../cart/cartSlice"
 import DeleteItem from "../cart/DeleteItem"
+import UpdateItemsQuantity from "../cart/UpdateItemsQuantity"
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza
@@ -41,7 +42,15 @@ function MenuItem({ pizza }) {
             <p className="uppercase">Sold out</p>
           )}
 
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-6">
+              <UpdateItemsQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button type="small" onClick={addToCart}>
